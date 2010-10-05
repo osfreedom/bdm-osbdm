8b25b2127b3cdf59c3eceafc8331dceb973b942d
Philippe De Muyter <phdm@macqel.be>
2010-10-05 18:33:30 +0200

    m68k/driver: Finish renaming of bdm_cf_pe_init_self and friends
    The following commit ;
    	commit 77dd3a4f627dcae4ac1eabf819f77deadb6a9782
    	Author: Chris Johns <cjohns@users.sourceforge.net>
    	Date:   Thu Aug 12 13:32:57 2010 +1000
    
    	* driver/bdm-cf-pe.c, driver/bdm-cpu32.c, driver/bdm-tblcf.c:
    	Change the init self call names and make public. Move some common
    	pieces into a more public place. Move out the USB init code.
    
    renamed cf_pe_init_self into bdm_cf_pe_init_self, and likewise for
    bdm_cpu32_pd_init_self and bdm_cpu32_icd_init_self.  But the drivers
    calling those functions were not updated.  Fix that.
    
    Signed-off-by: Philippe De Muyter <phdm@macqel.be>

--- m68k/driver/openbsd/openbsd-bdm.c
+++ m68k/driver/openbsd/openbsd-bdm.c
@@ -324,9 +324,9 @@ init_module ()
     self->delayTimer  = 0;
     
     switch (BDM_IFACE (minor)) {
-      case BDM_CPU32_PD:     cpu32_pd_init_self (self); break;
-      case BDM_CPU32_ICD:    cpu32_icd_init_self (self); break;
-      case BDM_COLDFIRE_PE:  cf_pe_init_self (self); break;
+      case BDM_CPU32_PD:     bdm_cpu32_pd_init_self (self); break;
+      case BDM_CPU32_ICD:    bdm_cpu32_icd_init_self (self); break;
+      case BDM_COLDFIRE_PE:  bdm_cf_pe_init_self (self); break;
       default:
         PRINTF ("BDM driver has no interface for minor number\n");
         cleanup_module ();
